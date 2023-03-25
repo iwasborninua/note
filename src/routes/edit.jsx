ecc68da093a13cbc97484bf7607c742a470f7f5c
Michael Kharchenko <mihail.macnamara@gmail.com>
2023-03-25 23:03:01 +0200

    search input

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -2,6 +2,7 @@ import {
     Form,
     useLoaderData,
     redirect,
+    useNavigate
 } from "react-router-dom";
 import {getContact, updateContact} from "../contacts.js";
 
@@ -19,6 +20,8 @@ export async function action({request, params}) {
 
 export default function EditContact() {
     const { contact } = useLoaderData();
+    const navigate = useNavigate();
+
     return (
         <Form method="post" id="contact-form">
             <p>
@@ -67,7 +70,12 @@ export default function EditContact() {
             </label>
             <p>
                 <button type="submit">Save</button>
-                <button type="button">Cancel</button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >Cancel</button>
             </p>
         </Form>
     );
